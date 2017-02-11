15ab03f678886bf0074e8658a03c3a46da2a7554
Andrew Gulik <andrewgulik@gmail.com>
2017-02-11 12:54:35 +0800

    added @media styles to make page fully responsive for smartphones

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 
-// this is the quote API
+// this is the quote api
 
 var quoteApi = "http://api.forismatic.com/api/1.0/?format=jsonp&method=getQuote&jsonp=?&lang=en";
-
-// this is the tweet API
-
 
 // this is the button event handler
 
@@ -12,21 +9,20 @@ $("button").click(function() {
   getQuote()
 });
 
-
-
-// this is the ajax request when the button is clicked
-
-// use $.getJSON function if information is already specified in the url
-// it is shorthand for the longer, $.ajax()
-// use $.ajax() if you need to manually list out preferences
+// this is the ajax request function when the button is clicked
+// use $.getJSON function if information is already specified in the api url
+// use $.ajax() if you need to manually list out data retrieval preferences
 
 function getQuote() {
   $.getJSON(quoteApi, function(response) {
+    // if the quote doesn't have an author, show 'uknown'
     if (response.quoteAuthor === '') {
       response.quoteAuthor = 'unknown';
     }
+    // create local variable for twitter api + link data to tweet
     var tweet = 'https://twitter.com/intent/tweet?text=' + response.quoteText + ' - ' + response.quoteAuthor;
 
+    // return the quote, author, tweet button and tweet functionality
     $("#actualquote").html(response.quoteText);
     $("#author").html('- '+response.quoteAuthor);
     $("#tweetshow").show();
