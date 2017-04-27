a4ed150c21181ff19b43fbc048f8fffa8e500d2c
Andrew Gulik <andrewgulik@gmail.com>
2017-04-27 10:06:26 -0400

    switched js file up, using promises instead of jquery, fixed cors proxy issue with heroku link

--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
 
 // this is the quote api
+var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+var QUOTE_API = 'http://api.forismatic.com/api/1.0/?format=json&method=getQuote&jsonp=?&lang=en';
 
-var quoteApi = "http://api.forismatic.com/api/1.0/?format=jsonp&method=getQuote&jsonp=?&lang=en";
+// the api call function
 
-// this is the button event handler
+function getQuote() {
+
+  var url = `${CORS_PROXY}${QUOTE_API}`;
+  return (
+      fetch(url)
+      .then(response => response.json())
+      .then(data => data)
+    );
+
+}
+
+// button event handler and data presentation
 
 $("button").click(function() {
   getQuote()
-});
-
-// this is the ajax request function when the button is clicked
-// use $.getJSON function if information is already specified in the api url
-// use $.ajax() if you need to manually list out data retrieval preferences
-
-function getQuote() {
-  $.getJSON(quoteApi, function(response) {
-    // if the quote doesn't have an author, show 'uknown'
+  .then(function(response) {
     if (response.quoteAuthor === '') {
       response.quoteAuthor = 'unknown';
     }
-    // create local variable for twitter api + link data to tweet
     var tweet = 'https://twitter.com/intent/tweet?text=' + response.quoteText + ' - ' + response.quoteAuthor;
-
-    // return the quote, author, tweet button and tweet functionality
     $("#actualquote").html(response.quoteText);
     $("#author").html('- '+response.quoteAuthor);
     $("#tweetshow").show();
     $("#tweetit").attr("href", tweet);
   })
-}
+});
